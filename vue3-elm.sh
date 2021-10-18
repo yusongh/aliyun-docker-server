49d18933db9a909d8fb873feaebcf580d1413606
yusongH <549962989@qq.com>
2021-10-18 22:55:27 +0800

    update

--- vue3-elm.sh
+++ vue3-elm.sh
@@ -4,9 +4,9 @@ echo -e "---------docker Stop--------"
 docker stop vue3-elm
 echo -e "---------docker Rm--------"
 docker rm vue3-elm
-docker rmi registry.cn-hangzhou.aliyuncs.com/zlyyyy/bilibili-vue:latest
+docker rmi registry.cn-hangzhou.aliyuncs.com/yusongh/vue3-elm:latest
 echo -e "---------docker Pull--------"
-docker pull registry.cn-hangzhou.aliyuncs.com/zlyyyy/bilibili-vue:latest
+docker pull registry.cn-hangzhou.aliyuncs.com/yusongh/vue3-elm:latest
 echo -e "---------docker Create and Start--------"
-docker run --rm -d -p 8080:80 --name vue3-elm registry.cn-hangzhou.aliyuncs.com/zlyyyy/bilibili-vue:latest
+docker run --rm -d -p 8080:80 --name vue3-elm registry.cn-hangzhou.aliyuncs.com/yusongh/vue3-elm:latest
 echo -e "---------deploy Success--------"
