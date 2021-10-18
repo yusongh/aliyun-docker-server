fd6450f37c658ffb9d3d46250e70e0d766152373
yusongH <549962989@qq.com>
2021-10-18 22:32:28 +0800

    update

---
+++ vue3-elm.sh
@@ -0,0 +1,12 @@
+echo -e "---------docker Login--------"
+docker login --username=$1 registry.cn-hangzhou.aliyuncs.com --password=$2
+echo -e "---------docker Stop--------"
+docker stop vue3-elm
+echo -e "---------docker Rm--------"
+docker rm vue3-elm
+docker rmi registry.cn-hangzhou.aliyuncs.com/zlyyyy/bilibili-vue:latest
+echo -e "---------docker Pull--------"
+docker pull registry.cn-hangzhou.aliyuncs.com/zlyyyy/bilibili-vue:latest
+echo -e "---------docker Create and Start--------"
+docker run --rm -d -p 8080:80 --name vue3-elm registry.cn-hangzhou.aliyuncs.com/zlyyyy/bilibili-vue:latest
+echo -e "---------deploy Success--------"
